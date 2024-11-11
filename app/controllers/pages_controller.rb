eaadfaf452cbcb360e57b4bf2b57f9e3f59ab3bc
Maxime Lazzarotto <lazzarottomaxime@gmail.com>
2024-11-11 12:57:15 +0100

    ex2 update

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+# pages controller
+class PagesController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = if params[:input] == "I'm going to work"
+                'Great!'
+              elsif params[:input] == '?'
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
